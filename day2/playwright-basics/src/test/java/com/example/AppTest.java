eba2f92236a3f6f53e3323cccde70295ae626b1b
Nag <nagcloudlab@gmail.com>
2024-07-07 14:45:45 +0530

    locators

--- day2/playwright-basics/src/test/java/com/example/AppTest.java
+++ day2/playwright-basics/src/test/java/com/example/AppTest.java
@@ -1,20 +1,127 @@
 package com.example;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 
 public class AppTest {
 
-    @Test
-    public void appTest() {
+//    @Test
+    public void test1() {
         try(Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("msedge"));
             Page page = browser.newPage();
             page.navigate("https://playwright.dev/");
             System.out.println(page.title());
+            browser.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    @Test
+    public void test2() {
+        try(Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("msedge"));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("https://playwright.dev/");
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+//            Locator getStarted=page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Get Started"));
+            Locator getStarted = page.locator("text=Get Started");
+            assertThat(getStarted).hasAttribute("href", "/docs/intro");
+            getStarted.click();
+            assertThat(page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Installation"))).isVisible();
+            browser.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    @Test
+    public void test3() {
+        try(Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("msedge"));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("http://localhost:5500/");
+
+//            Locator locator=page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign in"));
+//            locator.hover();
+//            locator.click();
+
+//            assertThat(page
+//                    .getByRole(AriaRole.HEADING,
+//                            new Page.GetByRoleOptions().setName("Sign up")))
+//                    .isVisible();
+//
+//            page.getByRole(AriaRole.CHECKBOX,
+//                            new Page.GetByRoleOptions().setName("Subscribe"))
+//                    .check();
+//
+//            page.getByRole(AriaRole.BUTTON,
+//                            new Page.GetByRoleOptions().setName(
+//                                    Pattern.compile("submit", Pattern.CASE_INSENSITIVE)))
+//                    .click();
+
+//            page.getByLabel("Password").fill("secret");
+
+//            page.getByPlaceholder("name@example.com").fill("playwright@microsoft.com");
+
+
+//            assertThat(page.getByText("Welcome, John")).isVisible();
+
+//            assertThat(page
+//                    .getByText("Welcome, John", new Page.GetByTextOptions().setExact(true)))
+//                    .isVisible();
+
+//            assertThat(page
+//                    .getByText(Pattern.compile("welcome, john$", Pattern.CASE_INSENSITIVE)))
+//                    .isVisible();
+
+//            page.getByAltText("playwright logo").click();
+
+//            assertThat(page.getByTitle("Issues count")).hasText("25 issues");
+
+//            page.getByTestId("directions").click();
+
+//            playwright.selectors().setTestIdAttribute("data-pw");
+
+//            page.getByTestId("directions").click();
+
+
+//            Locate by CSS or XPath
+
+//            page.locator("css=button").click();
+//            page.locator("xpath=//button").click();
+//            page.querySelector(".box").click();
+
+//            page.getByRole(AriaRole.LISTITEM)
+//                            .filter(new Locator.FilterOptions().setHasText("Product 2"))
+//                                    .getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("Add to cart"))
+//                            .click();
+
+//            page.getByRole(AriaRole.LISTITEM)
+//                    .filter(new Locator.FilterOptions()
+//                            .setHasText(Pattern.compile("Product 2")))
+//                    .getByRole(AriaRole.BUTTON,
+//                            new Locator.GetByRoleOptions().setName("Add to cart"))
+//                    .click();
+
+
+//            assertThat(page.getByRole(AriaRole.LISTITEM)
+//                    .filter(new Locator.FilterOptions().setHasNotText("Out of stock")))
+//                    .hasCount(5);
+
+            assertThat(page
+                    .getByRole(AriaRole.LISTITEM)
+                    .filter(new Locator.FilterOptions().setHasNot(page.getByText("Product 2")))).hasCount(1);
+
             browser.close();
         } catch (Exception e) {
             e.printStackTrace();
--- day2/playwright-basics/src/test/java/com/example/TrelloTest.java
+++ day2/playwright-basics/src/test/java/com/example/TrelloTest.java
@@ -1,4 +1,84 @@
 package com.example;
 
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+
 public class TrelloTest {
+
+    private static Playwright playwright;
+    private static Browser browser;
+    static APIRequestContext requestContext;
+
+
+    @BeforeAll
+    public static void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        requestContext = playwright.request().newContext();
+        APIResponse response = requestContext.post("http://localhost:3000/api/reset");
+        response.dispose();
+    }
+
+    @AfterAll
+    public static void teardown() {
+        browser.close();
+        playwright.close();
+    }
+
+
+    @Test
+    void createNewBoardWithListAndCards() {
+        BrowserContext context = browser.newContext();
+        Page page = context.newPage();
+
+        // Load the app
+        page.navigate("http://localhost:3000/");
+
+        // Create a new board
+        page.locator("input[placeholder='Name of your first board']").fill("board1");
+        page.locator("input[placeholder='Name of your first board']").press("Enter");
+        assertThat(page.locator("[name='board-title']")).hasValue("board1");
+        assertThat(page.locator("input[placeholder='Enter list title...']")).isVisible();
+        assertThat(page.locator("[data-cy='list']")).not().isVisible();
+
+        // take a screenshot
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots/board1.1.png")));
+
+        // Create a new list
+        page.locator("input[placeholder='Enter list title...']").fill("list1");
+        page.locator("input[placeholder='Enter list title...']").press("Enter");
+        assertThat(page.locator("[data-cy='list-name']")).hasValue("list1");
+
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots/board1.2.png")));
+
+
+        // Add cards to the list
+        page.locator("text=Add another card").click();
+        page.getByPlaceholder("Enter a title for this card...").fill("Buy groceries", new Locator.FillOptions().setForce(true));
+        page.locator("button:has-text('Add card')").click();
+        page.getByPlaceholder("Enter a title for this card...").fill("Mow the lawn");
+        page.locator("button:has-text('Add card')").click();
+        page.getByPlaceholder("Enter a title for this card...").fill("Walk the dog");
+        page.locator("button:has-text('Add card')").click();
+        assertThat(page.locator("[data-cy='card-text']")).hasText(new String[]{"Buy groceries", "Mow the lawn", "Walk the dog"});
+
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots/board1.3.png")));
+
+        // Navigate to the home page
+        page.locator("role=navigation").locator("role=button").click();
+        assertThat(page.locator("text=My Boards")).isVisible();
+        assertThat(page.locator("text=board1")).isVisible();
+
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots/board1.4.png")));
+
+        context.close();
+
+    }
+
 }
